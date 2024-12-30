a3306f348604ebb950d67a0bb77e9796f537b9c7
scottishwidow <mike.voznyanskix@gmail.com>
2024-12-30 21:05:37 +0200

    api2 consumer.js

---
+++ api2/consumer.js
@@ -0,0 +1,20 @@
+const amqplib = require('amqplib');
+const queue = 'tasks';
+
+(async () => {
+    try {
+        const connection = await amqplib.connect('amqp://localhost');
+        const channel = await connection.createChannel();
+        await channel.assertQueue(queue);
+
+        console.log('Consumer 2 waiting for messages...');
+        channel.consume(queue, (msg) => {
+            if (msg !== null) {
+                console.log('Consumer 2 received:', msg.content.toString());
+                channel.ack(msg);
+            }
+        });
+    } catch (error) {
+        console.error('Error:', error);
+    }
+})();
